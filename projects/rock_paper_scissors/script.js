aad293eb2aada62963af579f972664b55b97134f
Ian <94473108+idallenbach@users.noreply.github.com>
2024-11-18 13:35:46 -0600

    Created variables, comparison rules and started logic.

--- projects/rock_paper_scissors/script.js
+++ projects/rock_paper_scissors/script.js
@@ -2,10 +2,18 @@
 let random = Math.floor(Math.random() * 3);
 let choice = ["Rock", "Paper", "Scissors"];
 let computerChoice = choice[random].toLowerCase();
-let player = prompt("Rock, Paper, Scissors").toLowerCase();
 
-if (player == computerChoice) {
-    console.log("yup");
-} else {
-    console.log("nope");
+
+// get player input and convert to lowercase
+let playerChoice = prompt('Please Choose: "Rock", "Paper", "Scissors"');
+
+// convert choices to lower case
+ let playerCompare = playerChoice.toLowerCase();
+ let computerCompare = computerChoice.toLowerCase();
+
+// compare player choice to commputer choice
+if(playerCompare === computerCompare) {
+    console.log = `It's a Tie!`;
+} else if(playerCompare === "rock" && computerCompare === "paper") {
+    console.log(`You Lose! ${computerChoice} beats ${playerChoice}`)
 }
